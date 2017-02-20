b56067b0461e4afafc130050675be88d7577afab
zertu <290055513@qq.com>
2017-02-20 15:44:43 +0800

    quicksort

--- src/quicksort.js
+++ src/quicksort.js
@@ -1,13 +1,13 @@
 function quicksort(arr) {
-    if (arr.length == 1)
+    if (arr.length <= 1)
         return arr;
     let flag = arr[Math.floor(arr.length / 2)], left = [], right = [];
-    for (let i in arr) {
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] < flag) {
-            right.push(arr[i]);
+            left.push(arr[i]);
         }
-        else {
-            left.push[arr[i]];
+        else if (arr[i] > flag) {
+            right.push(arr[i]);
         }
     }
     return quicksort(left).concat([flag], quicksort(right));
--- src/quicksort.ts
+++ src/quicksort.ts
@@ -1,15 +1,16 @@
 function quicksort(arr : number[]) : number[] {
-    if (arr.length == 1) 
+    if (arr.length <= 1) 
         return arr
     let flag = arr[Math.floor(arr.length / 2)],
         left = [],
         right = []
-    for (let i in arr) {
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] < flag) {
-            right.push(arr[i])
-        } else {
-            left.push[arr[i]]
+            left.push(arr[i]);
+        } else if (arr[i] > flag) {
+            right.push(arr[i]);
         }
     }
-    return quicksort(left).concat([flag], quicksort(right));
+
+    return quicksort(left).concat([flag], quicksort(right))
 }
