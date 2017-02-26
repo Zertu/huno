99543c85b26e11b993cb5df72d5fb35e7393f1a2
zertu <290055513@qq.com>
2017-02-26 14:59:00 +0800

    优化了一下能达到89%

--- src/leetcode/81/SearchinRotatedSortedArrayII.js
+++ src/leetcode/81/SearchinRotatedSortedArrayII.js
@@ -8,55 +8,34 @@
  * @return {boolean}
  */
 var search = function (nums, target) {
-    if (nums.length <= 3) {
-        for (var i = 0; i <= nums.length; i++) {
-            if (nums[i] == target) {
-                return true;
+    return ranks(nums, target);
+};
+var ranks = function (nums, target) {
+    var head = 0, tail = nums.length;
+    while (head < tail) {
+        var mid = Math.floor((head + tail) / 2);
+        if (nums[mid] == target) {
+            return true;
+        }
+        if (nums[mid] > nums[head]) {
+            if (target < nums[mid] && target >= nums[head]) {
+                tail = mid;
+            }
+            else {
+                head = mid + 1;
             }
         }
-        return false;
-    }
-    nums = quicksort(dedupe(nums));
-    return ranks(nums, target) >= 0;
-};
-var ranks = function (arr, key) {
-    var mid = Math.floor(arr.length / 2);
-    console.log(arr);
-    if (mid <= 1) {
-        for (var i = 0; i <= arr.length; i++) {
-            if (arr[i] == key) {
-                return 1;
+        else if (nums[mid] < nums[head]) {
+            if (target > nums[mid] && target < nums[head]) {
+                head = mid + 1;
+            }
+            else {
+                tail = mid;
             }
         }
-        return -1;
+        else {
+            head++;
+        }
     }
-    if (key < arr[mid - 1]) {
-        return ranks(arr.splice(0, mid), key);
-    }
-    else if (key > arr[mid - 1]) {
-        return ranks(arr.splice(mid, arr.length - mid), key);
-    }
-    else if (key === arr[mid - 1]) {
-        return mid - 1;
-    }
-    else {
-        return -1;
-    }
+    return false;
 };
-function dedupe(array) {
-    return Array.from(new Set(array));
-}
-function quicksort(arr) {
-    if (arr.length <= 1)
-        return arr;
-    var flag = arr[Math.floor(arr.length / 2)], left = [], right = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] < flag) {
-            left.push(arr[i]);
-        }
-        else if (arr[i] > flag) {
-            right.push(arr[i]);
-        }
-    }
-    return quicksort(left).concat([flag], quicksort(right));
-}
