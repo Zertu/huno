5e928a5fc6479e6e3dca93fbb6d1f06fcac4bef6
zertu <290055513@qq.com>
2017-02-27 14:26:10 +0800

    leetcode415

---
+++ src/leetcode/415/AddStrings.js
@@ -0,0 +1,59 @@
+// Given two non-negative integers num1 and num2 represented as string, return
+// the sum of num1 and num2.
+// Note: The length of both num1 and num2 is < 5100. Both num1 and num2 contains
+// only digits 0-9. Both num1 and num2 does not contain any leading zero. You
+// must not use any built-in BigInteger library or convert the inputs to integer
+// directly.
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+    var arr1 = num1.split(''), arr2 = num2.split('');
+    if (arr1.length > arr2.length) {
+        for (var i = 0; i < (arr1.length - arr2.length); i++) {
+            arr2.unshift(0);
+        }
+    }
+    else if (arr1.length < arr2.length) {
+        for (var i = 0; i < (arr2.length - arr1.length); i++) {
+            arr1.unshift(0);
+        }
+    }
+    return add(arr1, arr2);
+};
+function add(arr1, arr2) {
+    var i = arr1.length, shouldplus = false, temparr = [], result;
+    while (i > 0) {
+        if (shouldplus) {
+            result = arr1[i] - 0 + (arr2[i] - 0) + 1;
+            if (result < 10) {
+                shouldplus = false;
+                temparr.push(result + '');
+            }
+            else {
+                shouldplus = true;
+                if (i != 0) {
+                    temparr.push((result + '').charAt(1));
+                }
+                else {
+                    temparr.push(result + '');
+                }
+            }
+        }
+        else {
+            result = arr1[i] - 0 + (arr2[i] - 0);
+            if (result < 10) {
+                shouldplus = false;
+                temparr.push(result + '');
+            }
+            else {
+                shouldplus = true;
+                temparr.push((result + '').charAt(1));
+            }
+        }
+        i--;
+    }
+    return temparr.join('');
+}
